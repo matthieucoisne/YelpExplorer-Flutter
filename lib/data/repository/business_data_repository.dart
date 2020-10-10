4dc33c3beb846cd7514e214fb91c3c3a9defe23d
Matthieu Coisne <matthieu.coisne@gmail.com>
2020-10-10 00:49:02 -0400

    updated repository: added getBusinessDetails() and getBusinessReviews()

--- lib/data/repository/business_data_repository.dart
+++ lib/data/repository/business_data_repository.dart
@@ -1,17 +1,52 @@
 import 'package:yelpexplorer/domain/model/business.dart';
+import 'package:yelpexplorer/domain/model/review.dart';
 import 'package:yelpexplorer/utils/const.dart' as Const;
 import 'package:yelpexplorer/utils/network.dart';
 
 class BusinessDataRepository {
   Future<List<Business>> getBusinessList() async {
-    String url = "${Const.URL_REST}/businesses/search?term=sushi&location=montreal&sortBy=rating&limit=10";
-    dynamic json = await NetworkHelper(url).getData();
+    String url = "${Const.URL_REST}/businesses/search?term=sushi&location=montreal&sortBy=rating&limit=20";
 
-    List<Business> data = [];
-    for (Map business in json["businesses"]) {
-      data.add(Business.fromJson(business));
+    List<Business> businessList = [];
+    try {
+      dynamic json = await NetworkHelper(url).getData();
+      for (Map business in json["businesses"]) {
+        businessList.add(Business.fromJson(business, false));
+      }
+    } catch (e) {
+      print(e);
     }
 
-    return data;
+    return businessList;
+  }
+
+  Future<Business> getBusinessDetails(String businessId) async {
+    String url = "${Const.URL_REST}/businesses/$businessId";
+    dynamic json = await NetworkHelper(url).getData();
+
+    Business businessDetails;
+    try {
+      businessDetails = Business.fromJson(json, true);
+    } catch (e) {
+      print(e);
+    }
+
+    return businessDetails;
+  }
+
+  Future<List<Review>> getBusinessReviews(String businessId) async {
+    String url = "${Const.URL_REST}/businesses/$businessId/reviews";
+
+    List<Review> reviews = [];
+    try {
+      dynamic json = await NetworkHelper(url).getData();
+      for (Map review in json["reviews"]) {
+        reviews.add(Review.fromJson(review));
+      }
+    } catch (e) {
+      print(e);
+    }
+
+    return reviews;
   }
 }
