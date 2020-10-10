51764ccde1559527cd1b96e97b353813a2b60017
Matthieu Coisne <matthieu.coisne@gmail.com>
2020-10-10 00:49:18 -0400

    updated models and their factories

--- lib/domain/model/user.dart
+++ lib/domain/model/user.dart
@@ -1,4 +1,16 @@
 class User {
   String name;
   String imageUrl;
+
+  User(
+    this.name,
+    this.imageUrl,
+  );
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      json["name"],
+      json["image_url"] ?? "",
+    );
+  }
 }
--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -1,3 +1,18 @@
 const URL_REST = "https://api.yelp.com/v3";
 
 const API_KEY = "YOUR_API_KEY";
+
+const PATTERN_DATE_TIME = "yyyy-MM-dd HH:mm:ss";
+const PATTERN_DATE = "M/d/yyyy";
+const PATTERN_HOUR_MINUTE = "HHmm";
+const PATTERN_TIME = "h:mm a";
+
+const Map<int, String> days = {
+  0: "Monday",
+  1: "Tuesday",
+  2: "Wednesday",
+  3: "Thursday",
+  4: "Friday",
+  5: "Saturday",
+  6: "Sunday",
+}; // TODO i18n
