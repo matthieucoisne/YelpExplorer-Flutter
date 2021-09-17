d4f79328d9727657d6fffed14c3e764c99066613
Matthieu Coisne <matthieu.coisne@gmail.com>
2021-09-17 01:14:20 -0400

    removed the graphql fragments

--- lib/features/business/data/graphql/datasource/remote/business_graphql_datasource.dart
+++ lib/features/business/data/graphql/datasource/remote/business_graphql_datasource.dart
@@ -39,63 +39,61 @@ class BusinessGraphQLDataSource {
 @visibleForTesting
 class Queries {
   static final businessListQuery = gql("""
-    query BusinessList(\$term: String!, \$location: String!, \$sortBy: String!, \$limit: Int!) {
+    query BusinessList(\$term: String, \$location: String!, \$sortBy: String, \$limit:Int) {
       search(term: \$term, location: \$location, sort_by: \$sortBy, limit: \$limit) {
         total
         business {
-          ...BusinessSummary
+          id
+          name
+          photos
+          rating
+          review_count
+          location {
+            address1
+            city
+          }
+          price
+          categories {
+            title
+          }
         }
-      }
-    }
-    """)..definitions.toList().addAll(_Fragments.businessSummary.definitions);
-
-  static final businessDetailsQuery = gql("""
-    query BusinessDetails(\$id: String!) {
-      business(id: \$id) {
-        ...BusinessSummary
-        ...BusinessDetails
-      }
-    }
-    """)..definitions.toList().addAll(_Fragments.businessSummary.definitions + _Fragments.businessDetails.definitions);
-}
-
-class _Fragments {
-  static final businessSummary = gql("""
-    fragment BusinessSummary on Business {
-      id
-      name
-      photos
-      rating
-      review_count
-      location {
-        address1
-        city
-      }
-      price
-      categories {
-        title
       }
     }
     """);
 
-  static final businessDetails = gql("""
-    fragment BusinessDetails on Business {
-      display_phone
-      hours {
-        open {
-          day
-          start
-          end
-        }
-      }
-      reviews {
-        user {
-          name
-          image_url
-        }
-        text
+  static final businessDetailsQuery = gql("""
+    query BusinessDetails(\$id: String!) {
+      business(id: \$id) {
+        id
+        name
+        photos
         rating
-        time_created
+        review_count
+        location {
+          address1
+          city
+        }
+        price
+        categories {
+          title
+        }
+        display_phone
+        hours {
+          open {
+            day
+            start
+            end
+          }
+        }
+        reviews {
+          user {
+            name
+            image_url
+          }
+          text
+          rating
+          time_created
+        }
       }
     }
     """);
